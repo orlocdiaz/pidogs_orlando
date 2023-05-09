3f3903008d0b8d5230272ecaa0884216a9d22a37
orlocdiaz <luisorlandocarmonadiaz@gmail.com>
2023-05-09 08:41:05 -0600

    Para formulario

--- client/src/components/AddDog/AddDog.jsx
+++ client/src/components/AddDog/AddDog.jsx
@@ -215,7 +215,7 @@ const AddDog = () => {
             />
           </div>
           <div className={styles.tempsMenu}>
-            <p
+            <div
               className={styles.menuTitle}
               onClick={() => setShowTemps(!showTemps)}
             >
@@ -243,7 +243,7 @@ const AddDog = () => {
                   <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
                 </svg>
               )}
-            </p>
+            </div>
 
             <div
               className={
--- client/src/components/Filters/Order.jsx
+++ client/src/components/Filters/Order.jsx
@@ -1,29 +1,100 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import styles from "./Filters.module.css";
 import { order } from "../../redux/actions";
 
 const Order = () => {
   const dispatch = useDispatch();
+  const [auxBreed, setAuxBreed] = useState(true);
+  const [auxWeight, setAuxWeight] = useState(true);
 
-  const handleOrder = (event) => {
-    dispatch(order(event.target.value));
+  const handleOrderBreed = () => {
+    // dispatch(order(event.target.value));
+    auxBreed ? dispatch(order("BD")) : dispatch(order("BA"));
+    setAuxBreed(!auxBreed);
+  };
+
+  const handleOrderWeight = () => {
+    // dispatch(order(event.target.value));
+    auxWeight ? dispatch(order("WD")) : dispatch(order("WA"));
+    setAuxWeight(!auxWeight);
   };
 
   return (
     <div className={styles.orderContainer}>
-      <option value="BA" onClick={handleOrder}>
-        Breed Asc
-      </option>
-      <option value="BD" onClick={handleOrder}>
-        Breed Desc
-      </option>
-      <option value="WA" onClick={handleOrder}>
-        Weight Asc
-      </option>
-      <option value="WD" onClick={handleOrder}>
-        Weight Desc
-      </option>
+      {!auxBreed ? (
+        <div className={styles.orderBtn} onClick={handleOrderBreed}>
+          A Z
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="30"
+            height="30"
+            fill="currentColor"
+            className="bi bi-arrow-up"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fillRule="evenodd"
+              d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"
+            />
+          </svg>
+        </div>
+      ) : (
+        <div className={styles.orderBtn} onClick={handleOrderBreed}>
+          A Z
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="30"
+            height="30"
+            fill="currentColor"
+            className="bi bi-arrow-down"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fillRule="evenodd"
+              d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"
+            />
+          </svg>
+        </div>
+      )}
+      {!auxWeight ? (
+        <div className={styles.orderBtn} onClick={handleOrderWeight}>
+          KG
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="30"
+            height="30"
+            fill="currentColor"
+            className="bi bi-arrow-up"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fillRule="evenodd"
+              d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"
+            />
+          </svg>
+        </div>
+      ) : (
+        <div className={styles.orderBtn} onClick={handleOrderWeight}>
+          KG
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="30"
+            height="30"
+            fill="currentColor"
+            className="bi bi-arrow-down"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fillRule="evenodd"
+              d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"
+            />
+          </svg>
+        </div>
+      )}
+
+      {/* <option value="WA">Weight Asc</option>
+      <option value="WD">Weight Desc</option> */}
     </div>
   );
 };
